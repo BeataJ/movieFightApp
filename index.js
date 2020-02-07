f249afc6fbd9ca39b97916cbe4afa964e00f8f0b
beata <beata@emc2wave.com>
2020-02-07 15:18:53 -0800

    add async fetchData with axios

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apykey: '40eae946',
+      s: 'avengers'
+    }
+  });
+};
