add03dcd377bb5cfc4d6c332fe638a4cf62448c1
beata <beata@emc2wave.com>
2020-02-12 20:57:40 -0800

    add fetchData as argument to createAut..

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
-    params: {
-      apikey: '40eae946',
-      s: searchTerm
-    }
-  });
-
-  if (response.data.Error) {
-    return [];
-  }
-
-  return response.data.Search;
-};
-
 createAutoComplete({
   root: document.querySelector('.autocomplete'),
   renderOption: movie => {
@@ -27,6 +12,20 @@ createAutoComplete({
   },
   inputValue: movie => {
     return movie.Title;
+  },
+  fetchData: async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '40eae946',
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
   }
 });
 
