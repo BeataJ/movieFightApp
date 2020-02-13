6a58702be451d0988cbd13210c1b5ffbd64cfba3
beata <beata@emc2wave.com>
2020-02-12 16:48:23 -0800

    select #summary and add response.data

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const onMovieSelect = async movie => {
     }
   });
 
-  console.log(response.data);
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
