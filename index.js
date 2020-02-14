83817e3229c0312af2b5d41db0ee36bf1c39f125
beata <beata@emc2wave.com>
2020-02-14 15:55:16 -0800

    add ref to leftMovies rightMovies runComparison()

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const autoCompleteConfig = {
       ${movie.Title} (${movie.Year})
     `;
   },
-  onOptionSelect: movie => {
-    document.querySelector('.tutorial').classList.add('is-hidden');
-    onMovieSelect(movie);
-  },
   inputValue: movie => {
     return movie.Title;
   },
@@ -31,15 +27,25 @@ const autoCompleteConfig = {
 
 createAutoComplete({
   ...autoCompleteConfig,
-  root: document.querySelector('#left-autocomplete')
+  root: document.querySelector('#left-autocomplete'),
+  onOptionSelect: movie => {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
+  }
 });
 
 createAutoComplete({
   ...autoCompleteConfig,
-  root: document.querySelector('#right-autocomplete')
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect: movie => {
+    document.querySelector('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
+  }
 });
 
-const onMovieSelect = async movie => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '40eae946',
@@ -47,7 +53,17 @@ const onMovieSelect = async movie => {
     }
   });
 
-  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
+
+  if (side === 'left') {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
 };
 
 const movieTemplate = movieDetail => {
